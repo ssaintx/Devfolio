dfe86ab6d500bb5458a30ab0debfb6a798e9131f
ssaintx <saintx.git@gmail.com>
2024-09-30 00:45:47 +0500

    update 1.1.2

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -4,12 +4,21 @@ import { Project } from '@/db/db.model';
 import { useState, useEffect } from 'react';
 import { useTranslations } from 'next-intl';
 import { createProject, updateProject, getAllProjects, deleteProject } from '@/db/db.actions';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 
 const Page = () => {
     const t = useTranslations("Admin.Create");
 
     const [projects, setProjects] = useState<Project[]>([]);
     const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+
+    {/* 
+        TODO 
+        - Add form validation and make form as reusable component
+        - Add reusable hooks for handling and submitting and updating e.t.c
+        - Fix design and add more styles and animations
+    */}
 
     useEffect(() => {
         const fetchData = async () => {
@@ -54,68 +63,67 @@ const Page = () => {
     return (
         <section>
             <h1 className="heading_admin">{t("Heading")}</h1>
-            <div className="max-w-lg mx-auto p-6 border border-gray-300 rounded-lg bg-white shadow-md mt-8">
-                <h2 className="text-2xl font-semibold mb-4">Project Form</h2>
-                <form className="space-y-4" onSubmit={handleSubmit}>
-                    <input
+            <form className="flex flex-col space-y-4 mt-8" onSubmit={handleSubmit}>
+                <div className="flex flex-row gap-2">
+                    <Input
                         type="text"
                         name="title"
                         placeholder="Title"
                         value={selectedProject?.title || ''}
                         onChange={handleInputChange}
                         required
-                        className="w-full p-2 border border-gray-300 rounded"
+                        className="glassmorphism p-2 w-full rounded-lg bg-zinc-100"
                     />
-                    <input
+                    <Input
                         type="text"
                         name="subtitle"
                         placeholder="Subtitle"
                         value={selectedProject?.subtitle || ''}
                         onChange={handleInputChange}
                         required
-                        className="w-full p-2 border border-gray-300 rounded"
+                        className="glassmorphism p-2 w-full rounded-lg bg-zinc-100"
                     />
-                    <textarea
-                        name="description"
-                        placeholder="Description"
-                        value={selectedProject?.description || ''}
-                        onChange={handleInputChange}
-                        required
-                        className="w-full p-2 border border-gray-300 rounded"
-                    />
-                    <input
-                        type="text"
-                        name="imageURL"
-                        placeholder="Image URL"
-                        value={selectedProject?.imageURL || ''}
-                        onChange={handleInputChange}
-                        required
-                        className="w-full p-2 border border-gray-300 rounded"
-                    />
-                    <button type="submit" className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600">
-                        {selectedProject ? 'Update' : 'Create'}
-                    </button>
-                </form>
+                </div>
+                <Textarea
+                    name="description"
+                    placeholder="Description"
+                    value={selectedProject?.description || ''}
+                    onChange={handleInputChange}
+                    required
+                    className="glassmorphism p-2 w-full rounded-lg bg-zinc-100"
+                />
+                <Input
+                    type="text"
+                    name="imageURL"
+                    placeholder="Image URL"
+                    value={selectedProject?.imageURL || ''}
+                    onChange={handleInputChange}
+                    required
+                    className="glassmorphism p-2 w-full rounded-lg bg-zinc-100"
+                />
+                <div className="flex items-center justify-center w-full">
+                    <button type="submit" className="button px-12">{selectedProject ? 'Update' : 'Create'}</button>
+                </div>
+            </form>
 
-                <h2 className="text-2xl font-semibold mt-8 mb-4">Projects</h2>
-                <ul className="space-y-2">
-                    {projects.map((project) => (
-                        <li key={project.id} className="flex justify-between items-center p-2 border-b border-gray-200">
-                            <div>
-                                <strong>{project.title}</strong> - {project.subtitle}
-                            </div>
-                            <div className="space-x-2">
-                                <button onClick={() => handleEdit(project)} className="text-blue-500 hover:underline">
-                                    Edit
-                                </button>
-                                <button onClick={() => handleDelete(project.id!)} className="text-red-500 hover:underline">
-                                    Delete
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <h2 className="text-2xl font-semibold mt-8 mb-4">Projects</h2>
+            <ul className="space-y-2">
+                {projects.map((project) => (
+                    <li key={project.id} className="flex justify-between items-center p-2 border-b border-gray-100">
+                        <div>
+                            <strong>{project.title}</strong> - {project.subtitle}
+                        </div>
+                        <div className="space-x-2">
+                            <button onClick={() => handleEdit(project)} className="text-blue-500 hover:underline">
+                                Edit
+                            </button>
+                            <button onClick={() => handleDelete(project.id!)} className="text-red-500 hover:underline">
+                                Delete
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 };
